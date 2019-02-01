498a1ca8652e79260037708db2b0ffc32f591cfa
SamLiu <b10302017@mail.ntust.edu.tw>
2019-02-01 14:04:41 +0800

    Dd

--- docker_ugv_run.sh
+++ docker_ugv_run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-docker run -it \
+docker run -it --privileged \
            --net host \
-	   -v /dev:/dev \
+	   -v /dev/ttyS0:/dev/ttyS60 \
+	   -v /dev/ttyS1:/dev/ttyS61 \
+	   -v /dev/ttyS2:/dev/ttyS62 \
+	   -v /dev/ttyS3:/dev/ttyS63 \
+	   -v /dev/ttyS4:/dev/ttyS64 \
 	   -w /workspace/seekur_jr/catkin_ws coolcat647/seekur_jr bash
 
