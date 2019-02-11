f76e224a2a373aca9540c64767b584525e689d82
coolcat647 <coolcat647>
2019-02-11 08:12:18 -0800

    ddd

--- docker_ugv_run.sh
+++ docker_ugv_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker run -it --privileged \
            --net host \
-	   -v /dev/ttyS0:/dev/ttyS60 \
-	   -v /dev/ttyS1:/dev/ttyS61 \
-	   -v /dev/ttyS2:/dev/ttyS62 \
-	   -v /dev/ttyS3:/dev/ttyS63 \
-	   -v /dev/ttyS4:/dev/ttyS64 \
-	   -w /workspace/seekur_jr/catkin_ws coolcat647/seekur_jr bash
+	   -v /dev/ttyS0:/dev/ttyS0 \
+	   -v /dev/ttyS1:/dev/ttyS1 \
+	   -v /dev/ttyS2:/dev/ttyS2 \
+	   -v /dev/ttyS3:/dev/ttyS3 \
+	   -v /dev/ttyS4:/dev/ttyS4 \
+	   -w /workspace/seekur_jr/catkin_ws --name seekur coolcat647/seekur_jr bash
 
